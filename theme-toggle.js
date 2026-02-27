c76e4cfebcf20b713e8c5b60e785acb50541b95c
Seth Forte <sethforte1@gmail.com>
2026-02-26 19:49:57 -0600

    push changes

--- theme-toggle.js
+++ theme-toggle.js
@@ -60,7 +60,7 @@
 
     function applyTheme(t) {
         document.documentElement.setAttribute('data-theme', t);
-        toggle.innerHTML = t === 'dark' ? '\u2600' : '\u263E';
+        toggle.innerHTML = t === 'dark' ? '\u2600\uFE0F' : '\uD83C\uDF19';
         localStorage.setItem('theme', t);
     }
 
